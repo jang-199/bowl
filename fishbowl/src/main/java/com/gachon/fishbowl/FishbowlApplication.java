c85c28ff82b91e228c010b5c0407a09769b6ae7a
jang-199 <jw1010110@gmail.com>
2022-11-22 03:37:16 +0900

    Initial commit

---
+++ fishbowl/src/main/java/com/gachon/fishbowl/FishbowlApplication.java
@@ -0,0 +1,13 @@
+package com.gachon.fishbowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishbowlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishbowlApplication.class, args);
+	}
+
+}
---
+++ fishbowl/src/test/java/com/gachon/fishbowl/FishbowlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachon.fishbowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishbowlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
